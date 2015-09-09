fa4517b37b83ee4bd1030c03dc1defcd8316c6de
Nuke Sparrow <nukesparrow@bitmessage.ch>
2015-09-10 00:11:33 +0400

    copyrights todo

--- src/main/java/com/bb/htmlunit/DataUrlXmlSerializer.java
+++ src/main/java/com/bb/htmlunit/DataUrlXmlSerializer.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// TODO : Import copyrights from HtmlUnit serializer
+
 package com.bb.htmlunit;
 
 import java.io.IOException;
